0354771cbd275eb339a5df8e55e39a09eaa3de63
Jordan Hayashi <jhhayashi@gmail.com>
2018-02-25 15:42:49 -0500

    Add code to build off of in lecture 4

---
+++ lecture/4-lists-input/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
+import {Constants} from 'expo'
+
+import contacts from './contacts'
+
+export default class App extends React.Component {
+  state = {
+    showContacts: false,
+  }
+
+  toggleContacts = () => {
+    this.setState(prevState => ({showContacts: !prevState.showContacts}))
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title="toggle contacts" onPress={this.toggleContacts} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Constants.statusBarHeight,
+  },
+});
