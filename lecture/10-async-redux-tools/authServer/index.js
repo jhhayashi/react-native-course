a104df445d426acd645fe9da07abace07b99ed21
Jordan Hayashi <jhhayashi@gmail.com>
2018-04-16 18:53:13 -0400

    Add mock token in authServer response

--- lecture/10-async-redux-tools/authServer/index.js
+++ lecture/10-async-redux-tools/authServer/index.js
@@ -19,7 +19,7 @@ app.post('*', (req, res) => {
   // an attacker can probe the server to find all of the usernames.
   if (!users[username]) return res.status(403).send('User does not exist')
   if (users[username] !== password) return res.status(403).send('Incorrect password')
-  return res.status(200).send()
+  return res.json({token: 'thisIsAToken'})
 })
 
 // catch 404
